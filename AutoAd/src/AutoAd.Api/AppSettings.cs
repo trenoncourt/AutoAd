44719fc48e0595596f16d8f6bce3c4356c459ef8
Thibaut Renoncourt <trenoncourt@access-it.fr>
2017-09-07 15:16:03 +0200

    Init project

---
+++ AutoAd/src/AutoAd.Api/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoAd.Api
+{
+    public class AppSettings
+    {
+        public CorsSettings Cors { get; set; }
+    }
+
+    public class CorsSettings
+    {
+        public bool Enabled { get; set; }
+
+        public string Methods { get; set; }
+
+        public string Origins { get; set; }
+
+        public string Headers { get; set; }
+    }
+}
---
+++ AutoAd/src/AutoAd.Api/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace AutoAd.Api
+{
+    public class Program
+    {
+        private static AppSettings AppSettings;
+
+        public static void Main()
+        {
+            BuildWebHost().Run();
+        }
+
+        public static IWebHost BuildWebHost() =>
+            new WebHostBuilder()
+                .UseKestrel(options => options.AddServerHeader = false)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                    AppSettings = config.Build().Get<AppSettings>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    if (hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                        logging.AddConsole();
+                    }
+                })
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddRouting();
+                    if (AppSettings.Cors.Enabled)
+                    {
+                        services.AddCors();
+                    }
+                })
+                .Configure(app =>
+                {
+                    app.UseRouter(r =>
+                    {
+                        r.MapGet("users", async context =>
+                        {
+                            await context.Response.WriteAsync("");
+                        });
+                    });
+                })
+                .Build();
+    }
+}
+
